7f928c096ca310454d4f72d97f3932636a35c316
Felipe Cofre <felipecof97@gmail.com>
2021-07-08 03:28:05 -0400

    ultimos cambios..  rest api , tokens
    tokens, rest api

--- ProyectosDjangoGroundZeroTerminado/GroundZero/rest_obra/urls.py
+++ ProyectosDjangoGroundZeroTerminado/GroundZero/rest_obra/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from rest_obra.views import lista_obras,detalle_obras
+from rest_obra.viewslogin import loginn
 
 urlpatterns =[
     path('lista_obras', lista_obras , name= "lista_obras"),
     path('detalle_obras/<id>',detalle_obras, name= "detalle_obras"),
+    path('loginn',loginn, name="loginn"),
 ]
--- ProyectosDjangoGroundZeroTerminado/GroundZero/rest_obra/views.py
+++ ProyectosDjangoGroundZeroTerminado/GroundZero/rest_obra/views.py
@@ -1,6 +1,7 @@
+from django.contrib.auth.hashers import check_password
 from django.shortcuts import render
 from rest_framework import status   
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,permission_classes
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
@@ -9,8 +10,12 @@ from .serializers import ObrasSerializer
 from django.views import View
 from django.http import JsonResponse
 
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
 @csrf_exempt
 @api_view(['GET', 'POST'])
+@permission_classes((IsAuthenticated,))
 
 def lista_obras(request):
     #LISTA OBRAS
@@ -28,8 +33,9 @@ def lista_obras(request):
         else:
             return Response(serializer.errors, status=status.HTTP_201_CREATED)
 
-##
+
 @api_view(['GET', 'PUT','DELETE'])
+@permission_classes((IsAuthenticated,))
 def detalle_obras(request,id):
 
     try:
